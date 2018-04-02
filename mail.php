ebe2bbcdbfdd49cfda8eb3ba77444245f15f5fae
Spencer Hayes <spencerhayes@dhcp-wifi-8021x-155-41-66-184.bu.edu>
2018-04-02 17:36:33 -0400

    Hotfix

--- mail.php
+++ mail.php
@@ -1,10 +1,17 @@
-<?php $name = $_POST['name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
-$formcontent="From: $name \n Message: $message";
-$recipient = "SpencerHHayes@gmail.com";
-$subject = "Contact Form";
-$mailheader = "From: $email \r\n";
-mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!";
+<?php
+if(isset($_POST['submit'])){
+    $to = "spencerhhayes@gmail.com";
+    $from = $_POST['email'];
+    $name = $_POST['name'];
+    $subject = "Form submission";
+    $subject2 = "Copy of your form submission";
+    $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['message'];
+    $message2 = "Here is a copy of your message " . $name . "\n\n" . $_POST['message'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    mail($from,$subject2,$message2,$headers2);
+    echo "Mail Sent. Thank you ";
+    }
 ?>
